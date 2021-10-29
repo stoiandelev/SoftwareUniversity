7be5f8989362a5807e527a08fb1c2f940569437a
Stoian Delev <stoian.delev@yahoo.com>
2021-10-29 12:02:41 +0300

    Add files via upload

---
+++ 01. Java Basic/01. BasicProject more exercises /JavaFX/src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+import java.awt.*;
+
+public class Controller {
+
+    public TextField num1;
+    public TextField results;
+    public TextField num2;
+
+    public void calculate(ActionEvent actionEvent) {
+
+
+
+    }
+}
