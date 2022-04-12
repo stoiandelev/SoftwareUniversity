229794c6387415a9aa7d848f51832cfae5bc4aa0
Stoian Delev <stoian.delev@yahoo.com>
2022-04-12 21:23:19 +0300

    Add files via upload

---
+++ 07. Java DB MySQL/3. Basic CRUD - Lab/Basic CRUD-LAB.sql
@@ -0,0 +1,52 @@
+USE `hotel`;
+
+#1. Select Employee Information
+SELECT `id`,`first_name`, `last_name`, `job_title` FROM `employees`
+ORDER BY `id`;
+
+#2. Select Employees with Filter
+SELECT `id`, concat(`first_name`,' ', `last_name`) AS `full_name`,
+`job_title`, `salary` FROM `employees`
+WHERE `salary`> 1000;
+
+#3. Update Employees Salary
+UPDATE `employees`
+SET salary = salary + 100
+WHERE job_title =  'Manager';
+SELECT `salary` FROM `employees`;
+
+#4. Top Paid Employee
+SELECT * FROM `employees`
+ORDER BY `salary` DESC
+LIMIT 1;
+
+#5. Select Employees by Multiple Filters
+SELECT * FROM `employees`
+WHERE `department_id` = 4 AND `salary` >= 1000
+ORDER BY `id`;
+
+#6. Delete from Table
+DELETE FROM `employees`
+WHERE `department_id` = 1 OR `department_id` = 2;
+SELECT * FROM `employees`;
+
+#WHERE `department_id` IN(1, 2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
