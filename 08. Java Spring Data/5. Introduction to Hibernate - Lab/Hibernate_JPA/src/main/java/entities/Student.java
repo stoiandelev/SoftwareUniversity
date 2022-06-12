7c4865241dbfea87ef2be979f5172ae9973d4bd8
Stoian Delev <stoian.delev@yahoo.com>
2022-06-12 23:11:21 +0200

    Add files via upload

---
+++ 08. Java Spring Data/5. Introduction to Hibernate - Lab/Hibernate_JPA/src/main/java/entities/Student.java
@@ -0,0 +1,47 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+    public Student() {}
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
